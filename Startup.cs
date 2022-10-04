b356a14386884430142e66c796c0ca0650e86ea1
sahabutdinov.im <sahabutdinov.im@finist-soft.ru>
2022-10-04 11:27:46 +0300

    added caseRunner sevice

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,8 @@ namespace coin_test
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            //services.AddRazorPages();
+            services.AddHostedService<CaseRunner>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
