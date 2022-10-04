0a62dab2eb630b6ef2a83a5c49a4b8400d176c84
sahabutdinov.im <sahabutdinov.im@finist-soft.ru>
2022-10-04 14:42:44 +0300

    added countries and coordinates input

--- EuroDiffusion/CaseRunner.cs
+++ EuroDiffusion/CaseRunner.cs
@@ -13,12 +13,14 @@ namespace coin_test.EuroDiffusion
     {
         private ILogger<CaseRunner> Logger;
         readonly ICoinDiffusion CoinDiffusion;
+        public Dictionary<string, IList<int>> CountriesWithCoordinates;
+        public IList<ICountry> Countries;
+
         public CaseRunner(ICoinDiffusion coinDiffusion, ILogger<CaseRunner> logger)
         {
             this.CoinDiffusion = coinDiffusion;
-
             this.Logger = logger;
-
+            this.CountriesWithCoordinates= new Dictionary<string, IList<int>>();
         }
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
@@ -27,15 +29,51 @@ namespace coin_test.EuroDiffusion
 
         public async Task DoWork(CancellationToken cancellationToken)
         {
-            Dictionary<string, IList<int>> countriesWithCoordinates = new Dictionary<string, IList<int>>();
-
+            
             while (true)
             {
                 try
                 {
-                    this.Logger.LogInformation("test");
+                    Console.WriteLine($"Write countries count");
                     var countriesCount = uint.Parse(Console.ReadLine());
+                    Console.Clear();
+                    if (countriesCount <= 0)
+                    {
+                        this.Logger.LogInformation("Countries count are less than 1");
+                        break;
+                    }
                     this.CoinDiffusion.SetCountriesCount(countriesCount);
+
+                    for (int i = 1; i <= countriesCount; i++)
+                    {
+                        Console.WriteLine($"Country №{i} name");
+                        var countryName = Console.ReadLine();
+                        Console.Clear();
+
+                        Console.WriteLine($"Enter \"{countryName}\" XL coordinate");
+                        var coordinateXL = int.Parse(Console.ReadLine());
+                        Console.Clear();
+
+                        Console.WriteLine($"Enter \"{countryName}\" YL coordinate");
+                        var coordinateYL = int.Parse(Console.ReadLine());
+                        Console.Clear();
+
+                        Console.WriteLine($"Enter \"{countryName}\" XH coordinate");
+                        var coordinateXH = int.Parse(Console.ReadLine());
+                        Console.Clear();
+
+                        Console.WriteLine($"Enter \"{countryName}\" YH coordinate");
+                        var coordinateYH = int.Parse(Console.ReadLine());
+                        Console.Clear();
+
+                        IList<int> coordinates = new List<int>().ToList<int>();
+                        coordinates.Add(coordinateXL);
+                        coordinates.Add(coordinateYL);
+                        coordinates.Add(coordinateXH);
+                        coordinates.Add(coordinateYH);
+
+                        this.CountriesWithCoordinates.Add(countryName, coordinates);
+                    }
                 }
                 catch (Exception e)
                 {
