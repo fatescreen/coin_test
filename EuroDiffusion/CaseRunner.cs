23e4efdfe83db803495b4879b65b1aa865c19d1f
sahabutdinov.im <sahabutdinov.im@finist-soft.ru>
2022-10-10 11:32:01 +0300

    added input from file

--- EuroDiffusion/CaseRunner.cs
+++ EuroDiffusion/CaseRunner.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,23 +12,34 @@ namespace coin_test.EuroDiffusion
 {
     public class CaseRunner : BackgroundService
     {
+        private const char wordsSeparator = ' ';
+        private const string endOfFile = "0";
+        private const string inputPath = "input.config";
+        private const int countryNameIndex = 0;
+        private const int XLCoordinateIndex = 1;
+        private const int YLCoordinateIndex = 2;
+        private const int XHCoordinateIndex = 3;
+        private const int YHCoordinateIndex = 4;
+
         private ILogger<CaseRunner> Logger;
         private ICoinDiffusion CoinDiffusion;
         public Dictionary<string, IList<int>> CountriesWithCoordinates;
         public IList<ICountry> Countries;
+        public IList<Dictionary<string, IList<int>>> Cases;
 
         public CaseRunner(ILogger<CaseRunner> logger)
         {
             this.Logger = logger;
             this.CountriesWithCoordinates= new Dictionary<string, IList<int>>();
+            this.Cases = new List<Dictionary<string, IList<int>>>();
             this.Countries = new List<ICountry>();
         }
         protected async override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            this.CountriesWithCoordinates = ReadCase();
+            ReadCases();
             SolveCase(this.CountriesWithCoordinates);
 
-            Console.WriteLine(CountriesWithCoordinates.Keys.First());
+            Console.ReadLine();
         }
 
         public int SolveCase(Dictionary<string, IList<int>> countriesWithCoordinates)
@@ -51,56 +63,56 @@ namespace coin_test.EuroDiffusion
             return this.CoinDiffusion.DayOfDiffusion;
         }
 
-        public Dictionary<string, IList<int>> ReadCase()
-        {            
+        public IList<Dictionary<string, IList<int>>> ReadCases()
+        {
+            Dictionary<string, IList<int>> countriesWithCoordinates = new Dictionary<string, IList<int>>();
+
             try
             {
-                Console.WriteLine($"Write countries amount");
-                var countriesAmount = uint.Parse(Console.ReadLine());
-                Console.Clear();
-                if (countriesAmount <= 0)
+                using (StreamReader reader = new StreamReader(inputPath))
                 {
-                    this.Logger.LogInformation("Countries amount are less than 1");                    
+                    string? line;
+                    int caseAmount = 0;
+                    bool isCaseAmount = false;
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        isCaseAmount = int.TryParse(line, out caseAmount);
+                        if (line == endOfFile) 
+                        {
+                            Cases.Add(countriesWithCoordinates);
+                            break; 
+                        };
+
+                        if (isCaseAmount) 
+                        {
+                            if (countriesWithCoordinates.Count() > 0)
+                            {
+                                Cases.Add(countriesWithCoordinates);
+                                countriesWithCoordinates = new Dictionary<string, IList<int>>();
+                            }
+                            continue; 
+                        };
+
+                        var words = line.Split(wordsSeparator);
+
+                        IList<int> coordinates = new List<int>();
+                        coordinates.Add(int.Parse(words[XLCoordinateIndex]));
+                        coordinates.Add(int.Parse(words[YLCoordinateIndex]));
+                        coordinates.Add(int.Parse(words[XHCoordinateIndex]));
+                        coordinates.Add(int.Parse(words[YHCoordinateIndex]));
+
+                        countriesWithCoordinates.Add(words[countryNameIndex], coordinates);
+                    }                  
+
                 }
-
-                for (int i = 1; i <= countriesAmount; i++)
-                {
-                    Console.WriteLine($"Country №{i} name");
-                    var countryName = Console.ReadLine();
-                    Console.Clear();
-
-                    Console.WriteLine($"Enter \"{countryName}\" XL coordinate");
-                    var coordinateXL = int.Parse(Console.ReadLine());
-                    Console.Clear();
-
-                    Console.WriteLine($"Enter \"{countryName}\" YL coordinate");
-                    var coordinateYL = int.Parse(Console.ReadLine());
-                    Console.Clear();
-
-                    Console.WriteLine($"Enter \"{countryName}\" XH coordinate");
-                    var coordinateXH = int.Parse(Console.ReadLine());
-                    Console.Clear();
-
-                    Console.WriteLine($"Enter \"{countryName}\" YH coordinate");
-                    var coordinateYH = int.Parse(Console.ReadLine());
-                    Console.Clear();
-
-                    IList<int> coordinates = new List<int>().ToList<int>();
-                    coordinates.Add(coordinateXL);
-                    coordinates.Add(coordinateYL);
-                    coordinates.Add(coordinateXH);
-                    coordinates.Add(coordinateYH);
-
-                    this.CountriesWithCoordinates.Add(countryName, coordinates);
-                }
-                return CountriesWithCoordinates;
             }
             catch (Exception e)
             {
                 this.Logger.LogError("Wrong input", e);                    
             }               
             
-            return CountriesWithCoordinates;
+            return this.Cases;
         }
     }
 }
